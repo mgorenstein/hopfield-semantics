2eea4b593e81c324b5fb3033113fc6fa962eb031
Mark Gorenstein <markagorenstein@gmail.com>
2018-12-03 19:34:40 -0800

    train and test the network

--- train.py
+++ train.py
@@ -1,10 +1,19 @@
 import pickle
 from hopfieldnet.net import HopfieldNetwork
 from hopfieldnet.trainers import hebbian_training
+from scipy.spatial import distance
 
 # get ze data
 with open('vector_dict.pickle', 'rb') as fi:
     data = pickle.load(fi)
+
+def find_closest(output, input_patterns):
+    distances = []
+    for pattern in input_patterns:
+        euc_dist = distance.euclidean(output, pattern)
+        distances.append(euc_dist)
+    val, idx = min((val, idx) for (idx, val) in enumerate(distances))
+    return idx
 
 input_patterns = [data[cat]['category_vector'] for cat in data.keys()]
 
@@ -12,3 +21,8 @@ input_patterns = [data[cat]['category_vector'] for cat in data.keys()]
 network = HopfieldNetwork(1200)
 # train the network
 hebbian_training(network, input_patterns)
+
+for i, pattern in enumerate(input_patterns):
+    output = network.run(pattern)
+    closest = find_closest(output, input_patterns)
+    print(i, closest)
